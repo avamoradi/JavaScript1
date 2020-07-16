6a176089058fc62c641524b3a5d985b99d7e61df
Ava Moradi <ava.moradi86@gmail.com>
2020-07-16 11:29:31 +0200

    reminder.js file added

--- Week1/homework/js-exercises/reminder.js
+++ Week1/homework/js-exercises/reminder.js
@@ -1,11 +1,11 @@
 let x = 7;
-x = x % 3;
+x = x % 3;// 7 = 2*3+1
 console.log("7 % 3 = " + x);
 
 let y = 21;
-y = y % 4;
+y = y % 4;// 21 = 5*4+1
 console.log("21 % 4 = " + y);
 
 let z = 13;
-z = z % 2;
+z = z % 2; //13 = 6*2+1
 console.log('13 % 2 = ' + z);
