6162fbf57ea1347ea4d1299b936f773567a4ff58
Ava Moradi <ava.moradi86@gmail.com>
2020-07-16 09:17:39 +0200

    helloworld.js file added

---
+++ Week1/homework/js-exercises/helloworld.js
@@ -0,0 +1,32 @@
+'use strict'
+
+let greeting1 = "Hello World!";
+console.log(greeting1);
+
+let greeting2 = "Hallo Wereld!";
+console.log(greeting2);
+
+let greeting3 = "Salut Lume";// romanian
+console.log(greeting3);
+
+let greeting4 = "Hej Verden";// danish
+console.log(greeting4);
+
+let greeting5 = "Olá Mundo!";//portuguese
+console.log(greeting5);
+
+let greeting6 = "Ciao mondo!";//italian
+console.log(greeting6);
+
+let greeting7 = "Привет мир!";//russian
+console.log(greeting7);
+
+let greeting8 = "Hallo Welt!"; //german
+console.log(greeting8);
+
+let greeting9 = "Bonjour le monde!";//french
+console.log(greeting9);
+
+let greeting10 = "Hai dunia!";//malay
+console.log(greeting10);
+
