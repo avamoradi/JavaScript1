8f97cb16474b2c7239ec9ece7c5530c7ec2ec98a
Ava Moradi <ava.moradi86@gmail.com>
2020-07-16 10:36:54 +0200

    animalarray.js file is added

---
+++ Week1/homework/js-exercises/animalarray.js
@@ -0,0 +1,8 @@
+let items = [];
+console.log("the value of the array is empty: " );
+console.log(items);
+let  animals = ['dog', 'cat', 'cow'];
+console.log(animals);
+animals.push('piglet');
+console.log(animals);
+
