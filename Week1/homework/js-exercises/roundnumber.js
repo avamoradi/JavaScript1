96f001ab00eac8544afadd546108aafc3db3decb
Ava Moradi <ava.moradi86@gmail.com>
2020-07-16 10:24:51 +0200

    the roundnumber.js file added

---
+++ Week1/homework/js-exercises/roundnumber.js
@@ -0,0 +1,12 @@
+let z = 7.25;
+console.log('the value of z is: ' + z );
+let a = Math.round(7.25);
+console.log('the value of a is: ' + a);
+
+if (a > z ) {
+ let highest = a ;
+} else {
+  highest = z
+}
+
+console.log("the highest number is: " + highest );
