c03592a0b1d60aea6a0db2a8320138d5bc9b7d96
Ava Moradi <ava.moradi86@gmail.com>
2020-07-16 11:15:33 +0200

    typechecker.js file is added

--- Week1/homework/js-exercises/typechecker.js
+++ Week1/homework/js-exercises/typechecker.js
@@ -1,12 +1,27 @@
-let a = 12;
+let a = "str";
 let b = "12";
 
-check(a, b);
+let c = {
+  name: 'ava',
+  age: 33
+}
 
-function check(a,b) {
-  if (a === b) {
-    console.log("the type of two variable is the same");
+let d = {
+  country: 'Netherland',
+  city: 'Amsterdam'
+}
+
+check(a, b);
+check(a, c);
+check(a, d);
+check(b, c);
+check(b, d);
+check(c, d);
+
+function check(x, y) {
+  if (typeof(x) == typeof(y)) {
+    console.log("same");
   } else {
-    console.log("the type of two variable IS NOT the same")
+    console.log("NOT the same")
   }
 }
