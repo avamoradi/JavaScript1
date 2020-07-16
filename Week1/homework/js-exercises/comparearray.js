16ae491fd0e1b2ac007c3fdd0893787349c446bc
Ava Moradi <ava.moradi86@gmail.com>
2020-07-16 17:00:39 +0200

    comparearray.js file added

---
+++ Week1/homework/js-exercises/comparearray.js
@@ -0,0 +1,8 @@
+var a = ['one', 'two', 'three', 'four'];
+var b = ['hello', 123,  true, false, 7.75, 'bye',20];
+
+if (a.length == b.length) {
+  console.log('they are the same') 
+} else {
+  console.log('two different size')
+}
