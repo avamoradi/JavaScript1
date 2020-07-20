5380b3aa45393d142314795812c24cd7da0cf829
Ava Moradi <ava.moradi86@gmail.com>
2020-07-20 09:53:57 +0200

    recipecards,js file added

--- Week2/homework/js-exercises/recipecard.js
+++ Week2/homework/js-exercises/recipecard.js
@@ -5,8 +5,14 @@ myMeal.mealName = "Cheese Sandwich";
 myMeal.Serves = 1;
 myMeal.Ingredients = ['2 slices white bread', '1 tablespoons butter', '1 slices Cheddar cheese'];
 
-console.log(myMeal);
 
-//for (const key of myMeal){
-  //console.log(key);
-//}
+const keys = Object.keys(myMeal);
+
+for (const key of keys){
+  console.log(key + " : " + myMeal[key]);
+}
+
+for (let i = 0; i < keys.length; i++ ) {
+  let key = keys[i];
+  console.log(key + " : " + myMeal[key]);
+}
