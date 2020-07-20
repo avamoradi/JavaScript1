f955d3766bd85921b2639a923d0a92a0c0769a96
Ava Moradi <ava.moradi86@gmail.com>
2020-07-20 09:35:00 +0200

    removecomma.js file and oddevenreport.js file is added

---
+++ Week2/homework/js-exercises/recipecard.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const myMeal = {};
+myMeal.mealName = "Cheese Sandwich";
+myMeal.Serves = 1;
+myMeal.Ingredients = ['2 slices white bread', '1 tablespoons butter', '1 slices Cheddar cheese'];
+
+console.log(myMeal);
+
+//for (const key of myMeal){
+  //console.log(key);
+//}
