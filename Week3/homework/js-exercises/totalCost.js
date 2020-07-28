942eeed3e99347b6847429ade301a863fd82ab65
Ava Moradi <ava.moradi86@gmail.com>
2020-07-28 15:00:05 +0200

    totalConst.js file added

---
+++ Week3/homework/js-exercises/totalCost.js
@@ -0,0 +1,26 @@
+'use strict'
+
+ const cartForParty = {
+  Nachos : 2 ,
+  Popcorn : 1.5,
+  Peanuts : 0.7,
+  Beer : 3,
+  chips : 1.3
+}
+
+function calculateTotalPrice (cartForParty) {
+//   let sum = 0;
+//  for ( let x in cartForParty) {
+//    sum += cartForParty[x];
+//  }
+//  console.log(sum);
+const values = Object.values(cartForParty);
+let sum = 0;
+for (const value of values) {
+ sum += value;
+}
+console.log('Total: € ' + sum);
+}
+
+calculateTotalPrice(cartForParty);
+
