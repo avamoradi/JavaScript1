f3ca8800514936fcd1f1afbecfc0f0f32123997b
Ava Moradi <ava.moradi86@gmail.com>
2020-07-28 10:47:43 +0200

    dogAge.js file added

---
+++ Week3/homework/js-exercises/dogAge.js
@@ -0,0 +1,9 @@
+'use strict'
+function calculateDogAge(x) {
+  let age = x * 7;
+  return ('Your doggie is ' + age + ' years old in dog years!');
+}
+
+console.log(calculateDogAge(3));
+console.log(calculateDogAge(7));
+console.log(calculateDogAge(15));
